3baf66f8e563ccf6c8eb1b72356e72c24212f065
RizkySaputra <62467810+rizkysaputra4@users.noreply.github.com>
2022-09-24 15:25:29 +0700

    update password

--- app/configuration/config.py
+++ app/configuration/config.py
@@ -1,4 +1,4 @@
 DEBUG=True
 FLASK_ENV="development"
-DATABASE_URL="postgresql://postgres:postgres@localhost/stonk"
+DATABASE_URL="postgresql://postgres:;h8-2.>J#~l#F+dWnJq_).#Eu5G;<e@localhost/stonk"
 TELEGRAM_BOT="5635709561:AAE6PAeoUeaf9j0o6hHCZPScRGLwnJAbb8c"
