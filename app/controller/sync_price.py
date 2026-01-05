19afffae00b73b24ff4c386fabf514363109772b
RIZKY SAPUTRA <rizkyysa@gmail.com>
2026-01-06 04:42:04 +0700

    revert changes

--- app/controller/sync_price.py
+++ app/controller/sync_price.py
@@ -3,9 +3,17 @@ from flask import Blueprint
 
 from app.model.response.sync_data_response import BaseResponse
 from app.service.get_recommendation_service import get_recommendation
+from app.service.sync_price_data_service import sync_price_data
 
 sync_price = Blueprint('yahoo_api', __name__)
 
+
+@sync_price.route("/sync-data", methods=["GET"])
+def sync_data_route():
+    sync_price_data()
+    resp = flask.Response(BaseResponse(data="Success").toJSON())
+    resp.headers["Content-Type"] = "application/json"
+    return resp
 
 @sync_price.route("/today-pick", methods=["GET"])
 def get_today_pick():
